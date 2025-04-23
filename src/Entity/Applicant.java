c63fe4e0a2f2e8c3168782cd4772228ea78e461f
sheri30 <sherilynleety@gmail.com>
2025-04-24 00:46:47 +0800

    Update Applicant.java

--- src/Entity/Applicant.java
+++ src/Entity/Applicant.java
@@ -1,15 +1,26 @@
 package Entity;
 
+/**
+ * Represents an HDB applicant in the system.
+ * Extends the User class with specific fields related to application and project participation.
+ */
 public class Applicant extends User {
 
     private Project project;
+
     private Application application;
+
     private String flatType;
 
-    public UserRole getRole() {
-        return UserRole.APPLICANT;
-    }
-
+    /**
+     * Constructs an Applicant with the specified details.
+     *
+     * @param name           the applicant's name
+     * @param nric           the applicant's NRIC
+     * @param age            the applicant's age
+     * @param maritalStatus  the applicant's marital status
+     * @param password       the applicant's account password
+     */
     public Applicant(String name, String nric, int age, String maritalStatus, String password) {
         super(name, nric, age, maritalStatus, password);
         this.project = null;
@@ -17,20 +28,68 @@ public class Applicant extends User {
         this.flatType = null;
     }
 
-    public boolean isSingle() { return super.getMaritalStatus().equals("SINGLE"); }
+    /**
+     * Gets the role of this user.
+     *
+     * @return the user role as APPLICANT
+     */
+    @Override
+    public UserRole getRole() {
+        return UserRole.APPLICANT;
+    }
+
+    /**
+     * Returns true if the applicant is single.
+     *
+     * @return true if marital status is "SINGLE", false otherwise
+     */
+    public boolean isSingle() {
+        return super.getMaritalStatus().equals("SINGLE");
+    }
+
+    /**
+     * Gets the project the applicant has applied to.
+     *
+     * @return the project associated with this applicant
+     */
     public Project getProject() {
         return project;
     }
-    public Application getApplication() {
-        return this.application;
-    }
-    public void setFlatType(String flatType) {
-        this.flatType = flatType + "-Room";
-    }
+
+    /**
+     * Sets the project the applicant is applying to.
+     *
+     * @param project the project to associate with this applicant
+     */
     public void setProject(Project project) {
         this.project = project;
     }
+
+    /**
+     * Gets the application submitted by the applicant.
+     *
+     * @return the application object
+     */
+    public Application getApplication() {
+        return this.application;
+    }
+
+    /**
+     * Sets the application for this applicant.
+     *
+     * @param application the application object
+     */
     public void setApplication(Application application) {
         this.application = application;
+    }
+
+    /**
+     * Sets the flat type the applicant is interested in.
+     * Automatically appends "-Room" to the type.
+     *
+     * @param flatType the flat type, e.g., "2" for "2-Room"
+     */
+    public void setFlatType(String flatType) {
+        this.flatType = flatType + "-Room";
     }
 }

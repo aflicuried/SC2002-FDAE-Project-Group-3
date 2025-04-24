0add737c9a51c84b15108ac96d0899e59ecda000
sheri30 <sherilynleety@gmail.com>
2025-04-24 09:39:07 +0800

    Update ApplicationView.java

--- src/View/ApplicationView.java
+++ src/View/ApplicationView.java
@@ -5,7 +5,17 @@ import Entity.Project;
 
 import java.util.List;
 
+/**
+ * Provides static methods to display and generate various views and reports 
+ * related to {@link Application} entities.
+ */
 public class ApplicationView {
+
+    /**
+     * Displays the details of a single application.
+     *
+     * @param application the application to display
+     */
     public static void displayApplication(Application application) {
         System.out.println("Applicant NRIC: " + application.getUser().getNric());
         System.out.println("Applicant Name: " + application.getUser().getName());
@@ -16,10 +26,21 @@ public class ApplicationView {
         System.out.println("Withdrawal: " + application.isWithdrawal());
     }
 
+    /**
+     * Displays a list of applications by invoking {@link #displayApplication(Application)} 
+     * on each application in the list.
+     *
+     * @param applications the list of applications to display
+     */
     public static void displayApplications(List<Application> applications) {
         applications.forEach(a -> displayApplication(a));
     }
 
+    /**
+     * Generates and displays a summary of application details if the application status is BOOKED.
+     *
+     * @param application the application for which to generate the summary
+     */
     public static void generate(Application application) {
         if (application.getStatus() == Application.ApplicationStatus.BOOKED) {
             System.out.println("Applicant Name: " + application.getUser().getName());
@@ -30,10 +51,22 @@ public class ApplicationView {
         }
     }
 
+    /**
+     * Generates a summary report for a list of applications, displaying details 
+     * only for applications with status BOOKED.
+     *
+     * @param applications the list of applications to generate the report for
+     */
     public static void generateReport(List<Application> applications) {
         applications.forEach(a -> generate(a));
     }
 
+    /**
+     * Generates and displays a receipt for an application if its status is BOOKED.
+     * Includes personal and project information.
+     *
+     * @param application the application for which to generate the receipt
+     */
     public static void generateReceipt(Application application) {
         if (application.getStatus() == Application.ApplicationStatus.BOOKED) {
             System.out.println("Applicant Name: " + application.getUser().getName());

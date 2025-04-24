8666e10897d3f7955b777198d12105a0d940f311
sheri30 <sherilynleety@gmail.com>
2025-04-24 09:41:05 +0800

    Update RegistrationView.java

--- src/View/RegistrationView.java
+++ src/View/RegistrationView.java
@@ -4,7 +4,18 @@ import Entity.Registration;
 
 import java.util.List;
 
+/**
+ * Provides static methods to display information related to {@link Registration} entities,
+ * which represent HDB officer registrations for project assignments.
+ */
 public class RegistrationView {
+
+    /**
+     * Displays the details of a single registration, including the registration ID,
+     * officer's NRIC and name, associated project, and registration status.
+     *
+     * @param registration the registration to display
+     */
     public static void displayRegistration(Registration registration) {
         System.out.println("Registration ID: " + registration.getId());
         System.out.println("Officer NRIC: " + registration.getOfficer().getNric());
@@ -13,6 +24,12 @@ public class RegistrationView {
         System.out.println("Status: " + registration.getStatus());
     }
 
+    /**
+     * Displays a list of registrations by invoking {@link #displayRegistration(Registration)} 
+     * on each registration in the list.
+     *
+     * @param registrations the list of registrations to display
+     */
     public static void displayRegistrations(List<Registration> registrations) {
         registrations.forEach(RegistrationView::displayRegistration);
     }
